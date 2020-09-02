fe551e56cf4f2124e24fd1b623cd47bff423caff
mmizbani <mizmatcat@gmail.com>
2020-09-01 18:13:24 -0600

    fixed onboarding

--- server/src/routes/protect.ts
+++ server/src/routes/protect.ts
@@ -187,7 +187,7 @@ router.post('/invite', (req, res) => {
     
         sg.send({
           to: invitedEmail,
-          from: 'hello@corner.so',
+          from: {'email': 'hello@corner.so', 'name' : 'Corner'},
           subject: `${name} invited you to join Corner`,
           text: 'Grab your own Corner of the Internet',
           html: `Your friend ${name} has invited you to join Corner, a place for young people with big ideas on the internet. <br/> <br/> Create your account by visiting <a href="https://corner.so">corner.so</a>.`,
