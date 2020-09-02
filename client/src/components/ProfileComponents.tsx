5c87e3edc9fd652327724cb8d15c554911ce3b75
mmizbani <mizmatcat@gmail.com>
2020-09-02 13:48:48 -0600

    fixed onboarding

--- client/src/components/ProfileComponents.tsx
+++ client/src/components/ProfileComponents.tsx
@@ -16,16 +16,24 @@ export const Headline: React.FC<HeadlineComponent> = ({ id, props }) => {
 
 type BioProps = { id: string, type: string, props: any, profile: Profile }
 export const Bio: React.FC<BioProps> = ({ id, props, profile }) => {
-	return (
-		<ComponentContainer column width={12}>
-			<H1>
-				About {profile.components.find(comp => comp.type === 'name')?.props.name.split(' ')[0]}
-			</H1>
-			<BioText>
-				{props.bio}
-			</BioText>
-		</ComponentContainer>
-	)
+	console.log("`" + props.bio + "`")
+	if (props.bio = null) {
+		return (
+				<ComponentContainer column width={12}>
+					<H1>
+						About {profile.components.find(comp => comp.type === 'name')?.props.name.split(' ')[0]}
+					</H1>
+					<BioText>
+						{props.bio}
+					</BioText>
+				</ComponentContainer>
+		)
+	} 
+	else {
+		return (
+			<p></p>
+		)
+	}
 }
 
 export const Headshot: React.FC<HeadshotComponent> = ({ id, props }) => {
--- client/src/pages/ProfilePage.tsx
+++ client/src/pages/ProfilePage.tsx
@@ -54,7 +54,7 @@ export const ProfilePage: React.FC = () => {
   
         <Header title={profile.components.find(component => component.type === 'name')?.props.name} />
   
-        <BodyContainer column width={mobile ? 11 : 6}>
+        <BodyContainer width={mobile ? 11 : 6}>
 
           <CenteredContainer column width={12}>
             { profile.components
@@ -87,7 +87,7 @@ export const ProfilePage: React.FC = () => {
 
         <Header title={'Profile Not Found'} />
 
-        <NotFoundContainer column width={mobile ? 11 : 6}>
+        <NotFoundContainer width={mobile ? 11 : 6}>
           <H1>
             This profile doesn't exist!
           </H1>
@@ -116,12 +116,11 @@ const PageContainer = styled(Div)`
   max-width: 100vw;
 	align-items: center;
   position: relative;
+  min-height: ${window.innerHeight+"px"};
 `
 
 const BodyContainer = styled(Div)`
-	min-height: ${window.innerHeight+"px"};
   padding-top: 51px;
-  margin-bottom: 60px;
 `
 
 const CenteredContainer = styled(Div)`
